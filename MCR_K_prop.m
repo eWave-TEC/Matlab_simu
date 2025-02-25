af1cda40338dac01760b50368fa6fb5b1980f36e
knga4e <71518613+knga4e@users.noreply.github.com>
2025-02-25 15:02:38 -0600

    agregar

---
+++ MCR_K_prop.m
@@ -0,0 +1,26 @@
+
+gain = [];
+Energy_e = [];
+
+
+for i = 0:1:100
+    prop_gain = i;
+    wecSim;
+    Ener = Output_energy.signals.values(end);
+
+    gain = [gain, prop_gain];
+    Energy_e = [Energy_e, Ener];
+end
+
+
+T = table(gain', Energy_e', 'VariableNames', {'Prop_gain', 'Energy_extracted'});
+
+
+writetable(T, 'Energy_extracted.csv')
+
+figure;                           % Crea una nueva ventana de figura
+plot(gain, Energy_e, 'b-', 'LineWidth', 2); % Grafica con línea azul y grosor 2
+xlabel('Prop Gain');              % Etiqueta del eje X
+ylabel('Energy Extracted(J)');       % Etiqueta del eje Y
+title('Relación entre Prop Gain y Energy Extracted'); % Título de la gráfica
+grid on; 
